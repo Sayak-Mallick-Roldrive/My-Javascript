df4cf81c7cada28d0d47b28740a287a4941e0ed7
Sayak-Mallick-Roldrive <sayak.roldrive@gmail.com>
2023-06-21 17:56:08 +0530

    added sort.js

---
+++ Array Method/sort.js
@@ -0,0 +1,7 @@
+const names = ['Price','Ghost','Soap','Alex','Gaz','Hadir','Rudolfo','Alexandro']
+names.sort();
+console.log(names);
+
+
+const numbers = [12,44,57,78,58,78,32,45,34,43,64,57,35,48,76,78];
+console.log(numbers.sort())
